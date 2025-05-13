b194eb1005281861aac8e8346da6e5e7790e1f82
Naveen pandian <npandian515@gmail.com>
2025-05-13 23:25:45 +0530

    refactor: remove className from body in RootLayout component

--- lensfe/app/layout.tsx
+++ lensfe/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
         {children}
       </body>
