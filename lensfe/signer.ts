db3f08e326fd713463312ade4f6412eb7e155421
Naveen pandian <npandian515@gmail.com>
2025-05-14 10:13:20 +0530

    feat: implement login functionality with private key validation and integrate signer with Lens Protocol

--- lensfe/signer.ts
+++ lensfe/signer.ts
@@ -1,3 +1,12 @@
+import { Wallet } from 'ethers';
 import { privateKeyToAccount } from "viem/accounts";
 
-export const signer = privateKeyToAccount(process.env.APP_PRIVATE_KEY);
+const privateKey = process.env.APP_PRIVATE_KEY;
+if (!privateKey || !privateKey.startsWith("0x")) {
+  throw new Error("APP_PRIVATE_KEY environment variable must be defined and start with '0x'");
+}
+
+const account = privateKeyToAccount(privateKey as `0x${string}`);
+const signer = new Wallet(privateKey);
+export { signer ,privateKey};
+
